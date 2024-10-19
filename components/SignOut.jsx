0fcf07db94f36a03a43807294c63041466a1091f
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2024-10-19 23:57:57 +0100

    dev changes

--- components/SignOut.jsx
+++ components/SignOut.jsx
@@ -1,16 +1,14 @@
 'use client'; // Ensure it's a client-side component
 import { signOut } from 'next-auth/react';
-import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 
 const Signout = () => {
-  const router = useRouter();
   const [isSigningOut, setIsSigningOut] = useState(false);
 
   const handleSignOut = async () => {
     setIsSigningOut(true); // Indicate sign-out process started
     await signOut({ redirect: false }); // Prevent automatic redirect
-    router.push('/'); // Manually redirect to home page
+    window.location.href = '/'; // Use window.location for redirection
   };
 
   // Ensure the router is ready before allowing sign-out
